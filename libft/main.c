212b2bd2dd74d31ae9dfc26666fd0e6e3ae11c18
Mohammed Ouadia <mouadia@2-C-2.42heilbronn.de>
2023-10-08 15:19:25 +0200

    libft

--- libft/main.c
+++ libft/main.c
@@ -6,23 +6,22 @@
 /*   By: mouadia <mouadia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:31:48 by mouadia           #+#    #+#             */
-/*   Updated: 2023/10/08 14:46:06 by mouadia          ###   ########.fr       */
+/*   Updated: 2023/10/08 15:16:30 by mouadia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 #include "libft.h"
 
 int main(void)
 {
-    char *c;
-    int number1, number2;
+    const char c;
+    char *str1;
+    const char str[] = "this is just a test";
 
-    c = "1245";
 
-
-    number1 = ft_atoi(c);
-    number2 = ft_strlen(c);
-    printf("this is th eresult = %d\n and th elen of it is %d\n", number1, number2);
+    str1 = strchr(str, c);
+    printf("this is just a test %s\n", str1);
     return 0;
 }
