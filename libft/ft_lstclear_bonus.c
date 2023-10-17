402645c09015704f824c089ff9c3b58bdd3d0075
Mohammed Ouadia <mouadia@3-C-7.42heilbronn.de>
2023-10-17 21:01:36 +0200

    update

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -6,13 +6,25 @@
 /*   By: mouadia <mouadia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:38:09 by mouadia           #+#    #+#             */
-/*   Updated: 2023/10/16 15:55:38 by mouadia          ###   ########.fr       */
+/*   Updated: 2023/10/17 20:47:56 by mouadia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstclear(t_list **lst, void (*del)(void*))
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*next;
 
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		next = (*lst)->next;
+		if ((*lst)->content)
+			del((*lst)->content);
+		free(*lst);
+		*lst = next;
+	}
+	lst = NULL;
 }
--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,13 +6,31 @@
 /*   By: mouadia <mouadia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:03:24 by mouadia           #+#    #+#             */
-/*   Updated: 2023/10/16 16:13:02 by mouadia          ###   ########.fr       */
+/*   Updated: 2023/10/17 20:50:36 by mouadia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*current_new;
+	t_list	*head;
+
+	if (!lst)
+		return (lst);
+	head = (t_list *)ft_calloc(1, sizeof(t_list));
+	current_new = head;
+	while (current_new)
+	{
+		current_new->content = f(lst->content);
+		lst = lst->next;
+		if (!lst)
+			break ;
+		current_new->next = (t_list *)ft_calloc(1, sizeof(t_list));
+		current_new = current_new->next;
+		if (!current_new)
+			ft_lstclear(&head, del);
+	}
+	return (head);
 }
